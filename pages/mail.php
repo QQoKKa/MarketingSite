3174989c424952299558190b97bc858016c4965a
Mateusz <balcerzakmateusz2@gmail.com>
2022-12-08 21:28:39 +0100

    testing php, some more reactive

---
+++ pages/mail.php
@@ -0,0 +1,21 @@
+<?php 
+
+$to = "test@growitupmarketing.com";
+$subject = "Test mail";
+$message = "Hello! This is a simple email message.";
+$from = "Test";
+$headers = "From:" . $from;
+mail($to,$subject,$message,$headers);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
